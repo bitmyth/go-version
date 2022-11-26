ab1d7e8ebed385480934725cbe8e4876ad922b5f
bitmyth <bitmyth@outlook.com>
2022-11-26 14:51:15 +0800

    version

--- version.go
+++ version.go
@@ -1,6 +1,10 @@
 package build
 
-var (
-	Version string
-	Commit  string
+import (
+	_ "embed"
 )
+
+//go:generate bash get_version.sh
+//go:embed version.txt
+var Version string
+var Commit string
